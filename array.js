ff6d602f82ea41aace406aa8aba3c1130011aa23
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-07-25 22:25:55 -0300

    Manipulando array

---
+++ array.js
@@ -0,0 +1,22 @@
+const time = ['gremio', 'flamengo', 'vasco'] // Lista de dados
+
+console.log('Acessando o time:', time[0]); // Acessa uma posição do array
+console.log('Tamanho do array:', time.length); // Tamanho do array
+
+
+// Manipulando array
+
+time.push('são paulo') // Adiciona um item no final do Array(lista)
+console.log(time);
+
+time.pop() // Apaga o último item do Array(lista)
+console.log(time);
+
+time.unshift('palmeiras') // Adiciona um item no começo do Array(lista)
+console.log(time);
+
+time.shift() // Remove um item no começo do Array(lista)
+console.log(time); 
+
+
+
