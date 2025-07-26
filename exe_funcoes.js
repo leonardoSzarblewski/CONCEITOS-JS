4c9f852b158b985c12bbc8e5f5831290652d1256
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-07-26 18:26:05 -0300

    Exercicio: função

---
+++ exe_funcoes.js
@@ -0,0 +1,19 @@
+// Exercicio 3 - Funções
+
+// 1. Função que soma dois números
+// 2. Deve conter parâmetros
+
+// Numero1: 20
+// Numero2: 10
+// Soma: 30
+
+function soma(a, b) {
+    const resultado = a + b
+
+    console.log('Numero1:', a);
+    console.log('Numero2:', b);
+    console.log('Soma:', resultado);
+    
+}
+
+soma(20, 10)
