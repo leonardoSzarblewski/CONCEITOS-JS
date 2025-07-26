a6467ed814898ebc481847a30091fb610416f720
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-07-25 22:10:12 -0300

    tipo de variaveis e estrutura de array, objeto e função

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+let nome = 'leo' // (LET) Pode ser alterado - string  
+nome = 'matheus'
+
+var idade = 20 // (VAR) Pode ser alterado - number
+idade = 19 
+
+const maior_idade = true // (CONST) Não pode ser alterado - boolean  
+
+
+// Array
+const paises = ['brasil', 'argentina', 'chile']
+
+// Objeto
+const celular = {
+    nome: 'iphone',
+    valor: 8000
+}
+
+// Função
+function soma(a, b) {
+    return a + b
+    
+}
